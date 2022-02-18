2cddbf2a9c0822df64e739b5f506a218c7230592
Taras Glek <taras@glek.net>
2022-02-18 16:36:36 +0000

    wip

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 ```
 sudo bpftrace -l '*syscalls*mmap*'  -v
 
-sudo bpftrace mmap_pagefault_snoop.bt -c `which ls`
+sudo bpftrace mmap_pagefault_snoop.bt -c `which ls` | grep '(' | ./process.py
 
 ```
---
+++ process.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def page_fault(comm, address, ip):
+    pass
+
+def mmap(comm, fd, fname, len, ret):
+    pass
+
+if __name__ == "__main__":
+    import sys
+    eval((sys.stdin.read()))
